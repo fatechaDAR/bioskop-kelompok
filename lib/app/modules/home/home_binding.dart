17d72be3e2a279fee0916c2b1d715b74d6e27e3f
techa <fatechaanggarahmatulloh@gmail.com>
2025-10-30 03:48:41 +0000

    tmdb

--- lib/app/modules/home/home_binding.dart
+++ lib/app/modules/home/home_binding.dart
@@ -1,9 +1,16 @@
+Dart
+
 import 'package:get/get.dart';
 import 'home_controller.dart';
+import '../../data/providers/tmdb_provider.dart'; // <-- IMPORT BARU
 
 class HomeBinding extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut<HomeController>(() => HomeController());
+    
+    // --- TAMBAHKAN INI ---
+    // Daftarkan TmdbProvider agar bisa di 'find' oleh Controller
+    Get.lazyPut<TmdbProvider>(() => TmdbProvider());
   }
 }
